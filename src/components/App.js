6997411d823b86323ad5dfd8afb34d8670e53726
polumnana <elena.shxx@gmail.com>
2022-09-20 18:53:43 +0300

    app.js draft

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,30 @@
-import logo from '../logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AboutMe from './AboutMe/AboutMe';
+import AboutProject from './AboutProject/AboutProject';
+import Footer from './Footer/Footer';
+import NotFound from './NotFound/NotFound';
+import Portfolio from './Portfolio/Portfolio';
+import Promo from './Promo/Promo';
+import Techs from './Techs/Techs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='page'>
+        <Routes>
+          <Route path='/' element={<Footer />} />
+          <Route path='/techs' element={<Techs />} />
+          <Route path='/aboutme' element={<AboutMe />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/aboutproject' element={<AboutProject />} />
+          <Route path='/promo' element={<Promo />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+              
+
+
     </div>
   );
 }
