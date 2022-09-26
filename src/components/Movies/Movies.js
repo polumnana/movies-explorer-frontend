ef993cb0626717f202230c2314b3ef33379e19ab
polumnana <elena.shxx@gmail.com>
2022-09-26 10:06:42 +0300

    add components to movies

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,17 @@
+import Header from "../Header/Header";
+import SearchForm from "../SearchForm/SearchForm";
+import Gallery from "../Gallery/Gallery";
+import Footer from "../Footer/Footer";
+
+function Movies() {
+    return (
+        <>
+            <Header />
+            <SearchForm />
+            <Gallery />
+            <Footer />
+        </>
+    )
+}
+
+export default Movies;
