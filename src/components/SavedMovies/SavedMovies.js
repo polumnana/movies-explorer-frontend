2e39f0def0312b6c67d0b7aed0f3204712e6c68c
polumnana <elena.shxx@gmail.com>
2022-09-26 10:10:34 +0300

    add components to savedMovies

---
+++ src/components/SavedMovies/SavedMovies.js
@@ -0,0 +1,17 @@
+import Header from "../Header/Header";
+import SearchForm from "../SearchForm/SearchForm";
+import Gallery from "../Gallery/Gallery";
+import Footer from "../Footer/Footer";
+
+function SavedMovies() {
+    return (
+        <>
+            <Header />
+            <SearchForm />
+            <Gallery />
+            <Footer />
+        </>
+    )
+}
+
+export default SavedMovies;
