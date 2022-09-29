056e13d7ab7d7761fb66e7141cb9d0347fdaa06c
polumnana <elena.shxx@gmail.com>
2022-09-29 19:29:59 +0300

    searchform ad

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -10,23 +10,22 @@ function SearchForm(props) {
         <section className="searchform">
             <div className="searchform__container">
                 <img className="searchform__img" src={Find} alt="Изображение лупы"></img>
-                <input 
-                className="searchform__input"
-                placeholder="Фильм"
-                type="search"
+                <input
+                    className="searchform__input"
+                    placeholder="Фильм"
+                    type="search"
                 ></input>
                 <button className="searchform__button-find">
                     <img className="searchform__button-find-img" src={FindButton} alt="Кнопка поиск"></img>
                 </button>
                 <div className="searchform__switch">
                     <input
-                    className="searchform__switch-checkbox"
-                    type="checkbox"
-                    name="switch"
-                    id="switch"/>
+                        className="searchform__switch-checkbox"
+                        type="checkbox"
+                        name="switch"
+                        id="switch" />
                     <label for="switch" className="searchform__switch-text">Короткометражки</label>
                 </div>
-
             </div>
         </section>
     )
