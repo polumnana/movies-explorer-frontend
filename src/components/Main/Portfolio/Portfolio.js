e7363219d649bf93647e043bd61601a3740fe189
polumnana <elena.shxx@gmail.com>
2022-09-30 18:15:52 +0300

    add links

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -10,19 +10,25 @@ function Portfolio(props) {
                     <div className="portfolio__link portfolio__line">
                         <h2 className="portfolio__text">Статичный сайт</h2>
                         <button className="portfolio__button">
-                            <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                            <a rel="noreferrer" target="_blank" href="https://github.com/polumnana/how-to-learn">
+                                <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                            </a>
                         </button>
                     </div>
                     <div className="portfolio__link portfolio__line">
                         <h2 className="portfolio__text">Адаптивный сайт</h2>
                         <button className="portfolio__button">
-                            <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                            <a rel="noreferrer" target="_blank" href="https://github.com/polumnana/russian-travel">
+                                <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                            </a>
                         </button>
                     </div>
                     <div className="portfolio__link">
                         <h2 className="portfolio__text">Одностраничное приложение</h2>
                         <button className="portfolio__button">
-                            <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                            <a rel="noreferrer" target="_blank" href="https://github.com/polumnana/react-mesto-api-full">
+                                <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                            </a>
                         </button>
                     </div>
                 </section>
