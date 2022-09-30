1aa061fe9c41af13f2f68c8ebee8f5199910a358
polumnana <elena.shxx@gmail.com>
2022-09-30 12:11:08 +0300

    add burger block

--- src/components/Header/HeaderAuthorized/HeaderAuthorized.js
+++ src/components/Header/HeaderAuthorized/HeaderAuthorized.js
@@ -1,27 +1,37 @@
 import { Link } from 'react-router-dom';
+import ButtonMenu from '../../../images/BurgerMenu.svg';
 
 
 
 function HeaderAuthorized() {
     return (
-        <section className="header__authorized">
-            <div className='header__films'>
-                <Link to="/movies"
-                    className="header__button-films header__button-films-text header__button-films-text_selected">
-                    Фильмы
-                </Link>
-                <Link
-                    to="saved-movies"
-                    className="header__button-saved-films header__button-films-text header__button-films-text_selected">
-                    Сохранённые фильмы
-                </Link>
-            </div>
+        <>
+            <section class="header__burger">
+                <button class="header__button-menu">
+                    <img className='header__img' alt="Меню" src={ButtonMenu}></img>
+                    <span class="header__burger-layer"></span>
+                </button>
+            </section>
 
-            <Link to="/"
-                className="header__button-account">
-                Аккаунт
-            </Link>
-        </section>
+            <section className="header__authorized">
+                <div className='header__films'>
+                    <Link to="/movies"
+                        className="header__button-films header__button-films-text header__button-films-text_selected">
+                        Фильмы
+                    </Link>
+                    <Link
+                        to="saved-movies"
+                        className="header__button-saved-films header__button-films-text header__button-films-text_selected">
+                        Сохранённые фильмы
+                    </Link>
+                </div>
+
+                <Link to="/"
+                    className="header__button-account">
+                    Аккаунт
+                </Link>
+            </section>
+        </>
     );
 }
 
