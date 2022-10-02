ed3d6d9e23bad44e6a100d7d00c923d1e8f606f9
polumnana <elena.shxx@gmail.com>
2022-10-02 19:46:52 +0300

    edits tags for portfolio

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -4,35 +4,39 @@ import Link from "../../../images/Link.svg";
 function Portfolio(props) {
     return (
         <section className="portfolio">
-            <div className="portfolio__container">
+            <article className="portfolio__container">
                 <h1 className="portfolio__title">Портфолио</h1>
-                <article className="portfolio__links">
-                    <div className="portfolio__link portfolio__line">
-                        <h2 className="portfolio__text">Статичный сайт</h2>
-                        <button className="portfolio__button" type="button">
-                            <a rel="noreferrer" target="_blank" href="https://github.com/polumnana/how-to-learn">
-                                <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
-                            </a>
-                        </button>
-                    </div>
-                    <div className="portfolio__link portfolio__line">
-                        <h2 className="portfolio__text">Адаптивный сайт</h2>
-                        <button className="portfolio__button" type="button">
-                            <a rel="noreferrer" target="_blank" href="https://github.com/polumnana/russian-travel">
-                                <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
-                            </a>
-                        </button>
-                    </div>
-                    <div className="portfolio__link">
-                        <h2 className="portfolio__text">Одностраничное приложение</h2>
-                        <button className="portfolio__button" type="button">
-                            <a rel="noreferrer" target="_blank" href="https://github.com/polumnana/react-mesto-api-full">
-                                <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
-                            </a>
-                        </button>
-                    </div>
-                </article>
-            </div>
+
+                <ul className="portfolio__items">
+                    <li className="portfolio__item portfolio__line">
+                        <a
+                            className="portfolio__link"
+                            rel="noreferrer"
+                            target="_blank"
+                            href="https://github.com/polumnana/how-to-learn">Статичный сайт
+                            <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                        </a>
+                    </li>
+                    <li className="portfolio__item portfolio__line">
+                        <a
+                            className="portfolio__link"
+                            rel="noreferrer"
+                            target="_blank"
+                            href="https://github.com/polumnana/russian-travel">Адаптивный сайт
+                            <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                        </a>
+                    </li>
+                    <li className="portfolio__item portfolio__line">
+                        <a
+                            className="portfolio__link"
+                            rel="noreferrer"
+                            target="_blank"
+                            href="https://github.com/polumnana/react-mesto-api-full">Одностраничное приложение
+                            <img className="portfolio__img" src={Link} alt="Ссылка на страницу"></img>
+                        </a>
+                    </li>
+                </ul>
+            </article>
 
         </section >
     );
