4e1df0d5223a874f061ded2494169652d7ce0dd6
polumnana <elena.shxx@gmail.com>
2022-10-02 21:50:05 +0300

    login fix styles

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,63 +7,65 @@ function Login(props) {
 
     return (
         <section className="login">
-            <Link
-                to="/"
-                className="login__logo">
-                <img className="login__img" src={Logo} alt="Логотип"></img>
-            </Link>
+            <article className="login__container">
+                <Link
+                    to="/"
+                    className="login__logo">
+                    <img className="login__img" src={Logo} alt="Логотип"></img>
+                </Link>
 
-            <h2 className="login__title">Рады видеть!</h2>
-            <form
-                className="login__form"
-                name="login" >
+                <h2 className="login__title">Рады видеть!</h2>
+                <form
+                    className="login__form"
+                    name="login" >
 
-                <label className="login__label">
-                    <h2 className="login__subtitle">E-mail</h2>
-                    <input
-                        className="login__input"
-                        type="email"
-                        id="login-email-input"
-                        placeholder="Email"
-                        name="email"
-                        minLength="4"
-                        maxLength="40"
-                        value=""
-                        required
-                    />
-                </label>
-                <span className="login__type-input-error email-input-error"></span>
+                    <label className="login__label">
+                        <h2 className="login__subtitle">E-mail</h2>
+                        <input
+                            className="login__input"
+                            type="email"
+                            id="login-email-input"
+                            placeholder="Email"
+                            name="email"
+                            minLength="4"
+                            maxLength="40"
+                            value=""
+                            required
+                        />
+                    </label>
+                    <span className="login__type-input-error email-input-error"></span>
 
-                <label className="login__label">
-                    <h2 className="login__subtitle">Пароль</h2>
-                    <input
-                        className="login__input"
-                        type="password"
-                        id="login-password-input"
-                        placeholder="Пароль"
-                        name="password"
-                        minLength="6"
-                        maxLength="200"
-                        value=""
-                        required />
-                </label>
-                <span className="login__input_type-error password-input-error"></span>
+                    <label className="login__label">
+                        <h2 className="login__subtitle">Пароль</h2>
+                        <input
+                            className="login__input"
+                            type="password"
+                            id="login-password-input"
+                            placeholder="Пароль"
+                            name="password"
+                            minLength="6"
+                            maxLength="200"
+                            value=""
+                            required />
+                    </label>
+                    <span className="login__input_type-error password-input-error"></span>
 
-                <section className="login__submit">
-                    <span className="login__submit_type-error login-submit-error"></span>
-                    <button
-                        className="login__submit-button"
-                        type="submit"
-                    >Войти
-                    </button>
-                </section>
+                    <section className="login__submit">
+                        <span className="login__submit_type-error login-submit-error"></span>
+                        <button
+                            className="login__submit-button"
+                            type="submit"
+                        >Войти
+                        </button>
+                    </section>
 
-                <div className="login__signin">
-                    <p className="login__signin-text">Ещё не зарегистрированы? </p>
-                    <Link to="/signup" className="login__signin-link">Зарегистрироваться</Link>
-                </div>
+                    <div className="login__signin">
+                        <p className="login__signin-text">Ещё не зарегистрированы? </p>
+                        <Link to="/signup" className="login__signin-link">Зарегистрироваться</Link>
+                    </div>
 
-            </form>
+                </form>
+            </article>
         </section>
     )
 }
