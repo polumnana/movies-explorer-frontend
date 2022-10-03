b7df61cd66efc3279dce857b86446717ea0b1099
polumnana <elena.shxx@gmail.com>
2022-10-03 19:56:19 +0300

    apiAuth

---
+++ src/utils/AuthApi.js
@@ -0,0 +1,63 @@
+class AuthApi {
+    constructor({baseUrl}) {
+        this._baseUrl = baseUrl;
+    }
+
+    //Регистрация
+    signup({name, email, password}) {
+        return fetch(`${this._baseUrl}/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+            })
+        }).then(this._checkResponse);//Проверка ответа
+    }
+
+    //Вход
+    signin({email, password}) {
+
+        return fetch(`${this._baseUrl}/signin`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        }).then(this._checkResponse);//Проверка ответа
+    }
+
+    //Проверка валидности токена и получения email для хедера
+    checkToken(token) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        }).then(this._checkResponse);//Проверка ответа
+    }
+
+    _checkResponse(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        // если ошибка, отклоняем промис
+        return Promise.reject(`Ошибка: ${res.status}`);
+    }
+}
+
+const apiAuth = new AuthApi({
+    baseUrl: 'https://polumnana.movies.api.nomorepartiesxyz.ru',
+});
+
+export default apiAuth;
+
+
+
