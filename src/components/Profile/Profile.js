d3ab89acbe702a98745911267b36048d50e164cd
polumnana <elena.shxx@gmail.com>
2022-10-03 19:37:52 +0300

    profile validate with hook and handle edit/submit

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,30 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useFormWithValidation } from '../../utils/validate.js';
+
 
 function Profile(props) {
+
+    const { values, handleChange, errors, isValid, resetForm } = useFormWithValidation();
+    const [classesListEditProfile, setClassesListEditProfile] = React.useState('profile__navigation_visible');
+    const [classesListSubmitProfile, setClassesListSubmitProfile] = React.useState('');
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        console.log(1);
+        // props.onSubmit({values.email, values.password});
+        setClassesListSubmitProfile('');
+        setClassesListEditProfile('profile__navigation_visible');
+    }
+
+    function handleEditProfile() {
+        setClassesListSubmitProfile('profile__submit_visible');
+        setClassesListEditProfile('');
+    }
 
     return (
         <section className="profile">
             <h2 className="profile__title">Привет, Виталий!</h2>
             <form
                 className="profile__form"
-                name="profile" >
+                name="profile"
+                onSubmit={handleSubmit} >
                 <label className="profile__label">
                     <h2 className="profile__subtitle">Имя</h2>
                     <input
                         className="profile__input"
                         type="name"
+                        onChange={handleChange}
                         id="profile-name-input"
                         placeholder="Введите ваше имя"
                         name="name"
                         minLength="2"
                         maxLength="30"
-                        value=""
+                        value={values.name}
                         required
-                        readonly
+                        disabled
                     />
                 </label>
-                <span className="profile__input_type-error name-input-error"></span>
+                <span className="profile__input_type-error name-input-error">{errors.name}</span>
 
 
                 <label className="profile__label">
@@ -32,24 +53,29 @@ function Profile(props) {
                     <input
                         className="profile__input"
                         type="email"
+                        onChange={handleChange}
                         id="profile-email-input"
                         placeholder="Email"
                         name="email"
                         minLength="4"
                         maxLength="40"
-                        value=""
+                        value={values.email}
                         required
-                        readonly
+                        disabled
                     />
                 </label>
-                <span className="profile__input_type-error email-input-error"></span>
+                <span className="profile__input_type-error email-input-error">{errors.email}</span>
 
-                <section className="profile__navigation">
-                    <button className="profile__button-edit" type="button">Редактировать</button>
+                <section className={`profile__navigation ${classesListEditProfile}`}>
+                    <button
+                        className="profile__button-edit"
+                        type="button"
+                        onClick={handleEditProfile}
+                    >Редактировать</button>
                     <Link to="/" className="profile__signout-link">Выйти из аккаунта</Link>
                 </section>
 
-                <section className="profile__submit">
+                <section className={`profile__submit ${classesListSubmitProfile}`}>
                     <span className="profile__submit_type-error profile-submit-error"></span>
                     <button
                         className="profile__submit-button"
