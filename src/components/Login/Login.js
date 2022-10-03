898fb9bd3fd0699687c2d4bd2b58bb081ef74668
Elena Safina <elena.shxx@gmail.com>
2022-10-03 09:25:12 +0300

    login validate with hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,45 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import Logo from '../../images/Logo.svg';
+import { useForm, useFormWithValidation } from '../../utils/validate.js';
 
 
 function Login(props) {
-    const [email, setEmail] = React.useState('');
-    const [errorEmail, setErrorEmail] = React.useState('');
-    const [isValidEmail, setValidityEmail] = React.useState(false);
 
-    const [password, setPassword] = React.useState('');
-    const [errorPassword, setErrorPassword] = React.useState('');
-    const [isValidPassword, setValidityPassword] = React.useState(false);
-
-
-    function handleChangeEmail(evt){
-        const input = evt.target;
-        setEmail(input.value);
-
-        setValidityEmail(input.validity.valid);
-        if (!isValidEmail) {
-          setErrorEmail(input.validationMessage);
-        } else {
-          setErrorEmail('');  
-        }
-    }
-
-    function handleChangePassword(evt){
-        const input = evt.target;
-        setPassword(input.value);
-
-        setValidityPassword(input.validity.valid);
-        if (!isValidPassword) {
-          setErrorPassword(input.validationMessage);
-        } else {
-          setErrorPassword('');  
-        }
-    }
+    const { values, handleChange, errors, isValid, resetForm } = useFormWithValidation();
 
     function handleSubmit(evt){
         evt.preventDefault();
-        props.onSubmit({email, password});
+        console.log(1);
+        // props.onSubmit({values.email, values.password});
     }
 
     return (
@@ -55,6 +27,7 @@ function Login(props) {
                 <form
                     className="login__form"
                     name="login" 
+                    on
                     onSubmit={handleSubmit}>
 
                     <label className="login__label">
@@ -62,33 +35,33 @@ function Login(props) {
                         <input
                             className="login__input"
                             type="email"
-                            onChange={handleChangeEmail}
+                            onChange={handleChange}
                             id="login-email-input"
                             placeholder="Email"
                             name="email"
                             minLength="4"
                             maxLength="40"
-                            value={email}
+                            value={values.email}
                             required
                         />
                     </label>
-                    <span className="login__type-input-error email-input-error">{errorEmail}</span>
+                    <span className="login__type-input-error email-input-error">{errors.email}</span>
 
                     <label className="login__label">
                         <h2 className="login__subtitle">Пароль</h2>
                         <input
                             className="login__input"
                             type="password"
-                            onChange={handleChangePassword}
+                            onChange={handleChange}
                             id="login-password-input"
                             placeholder="Пароль"
                             name="password"
                             minLength="6"
                             maxLength="200"
-                            value={password}
+                            value={values.password}
                             required />
                     </label>
-                    <span className="login__type-input-error password-input-error">{errorPassword}</span>
+                    <span className="login__type-input-error password-input-error">{errors.password}</span>
 
                     <section className="login__submit">
                         <span className="login__submit_type-error login-submit-error"></span>
