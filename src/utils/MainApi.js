fda88232f113b431b8ef8e5d9cdce4ca4572e27d
polumnana <elena.shxx@gmail.com>
2022-10-04 18:43:56 +0300

    move auth to main api

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -3,6 +3,64 @@ class MainApi {
         this._baseUrl = baseUrl;
     }
 
+    //Регистрация
+    signup({ name, email, password }) {
+        return fetch(`${this._baseUrl}/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+            })
+        }).then(this._checkResponse);//Проверка ответа
+    }
+
+    //Вход
+    signin({ email, password }) {
+
+        return fetch(`${this._baseUrl}/signin`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        }).then(this._checkResponse);//Проверка ответа
+    }
+
+    //Проверка валидности токена и получения email для хедера
+    checkToken(token) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        }).then(this._checkResponse);//Проверка ответа
+    }
+
+    fetchUserInfo() {
+        return fetch(this._baseUrl + '/users/me', {
+            headers: this._getHeaders(),
+        }).then(this._checkResponse);
+    }
+
+    updateUserInfo({ name, email }) {
+        return fetch(this._baseUrl + '/users/me', {
+            method: 'PATCH',
+            headers: this._getHeaders(),
+            body: JSON.stringify({
+                name: name,
+                email: email
+            })
+        }).then(this._checkResponse);
+    }
+
     fetchSavedMovies() {
         return fetch(this._baseUrl + '/movies', {
             headers: this._getHeaders(),
