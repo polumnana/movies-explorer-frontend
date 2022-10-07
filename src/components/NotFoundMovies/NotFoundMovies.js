9c67dd7ab10df7a976771f64989ac63f1654e899
polumnana <elena.shxx@gmail.com>
2022-10-08 00:42:24 +0300

    notfound movies

---
+++ src/components/NotFoundMovies/NotFoundMovies.js
@@ -0,0 +1,14 @@
+function NotFoundMovie() {
+
+  function name(){
+    //findResultClass ... "notfound-movies__text" : "notfound-movies__text-error"
+  }
+
+  return (
+    <section className="notfound-movies">
+      <h2 className={findResultClass}></h2>
+    </section>
+  );
+}
+
+export default NotFoundMovie;
