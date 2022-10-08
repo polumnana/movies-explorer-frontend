2a014dee2486302019c0a368af067ff6bbebad02
Elena <elena.shxx@gmail.com>
2022-10-08 23:27:36 +0300

    active link style

--- src/components/Header/HeaderAuthorized/HeaderAuthorized.js
+++ src/components/Header/HeaderAuthorized/HeaderAuthorized.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import ButtonMenu from '../../../images/BurgerMenu.svg';
 import HeaderBurger from '../HeaderBurger/HeaderBurger';
 
 function HeaderAuthorized() {
 
   const [classesListBurger, setClassesListBurger] = React.useState('');
+  const location = useLocation().pathname;
 
   function handleOpenBurger() {
     setClassesListBurger("burger burger_opened");
@@ -14,6 +15,18 @@ function HeaderAuthorized() {
   function handleCloseBurger() {
     setClassesListBurger("burger");
   }
+  const moviesTextClassList =
+    location === "/movies"
+      ? "header__button-films header__button-films-text header__button-films-text_selected"
+      : "header__button-films header__button-films-text";
+
+
+  const savedMoviesTextClassList =
+    location === "/saved-movies"
+      ? "header__button-saved-films header__button-films-text header__button-films-text_selected"
+      : "header__button-saved-films header__button-films-text";
+
+
 
   return (
     <>
@@ -34,12 +47,12 @@ function HeaderAuthorized() {
       <section className="header__authorized">
         <div className='header__films'>
           <Link to="/movies"
-            className="header__button-films header__button-films-text header__button-films-text_selected">
+            className={moviesTextClassList}>
             Фильмы
           </Link>
           <Link
             to="/saved-movies"
-            className="header__button-saved-films header__button-films-text header__button-films-text_selected">
+            className={savedMoviesTextClassList}>
             Сохранённые фильмы
           </Link>
         </div>
