a0296405d054a596be713bd55fcfdf6c809ace15
Elena <elena.shxx@gmail.com>
2022-10-08 04:07:17 +0300

    show more

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,6 +3,39 @@ import Movie from "../Movie/Movie";
 import NotFoundMovies from "../NotFoundMovies/NotFoundMovies";
 
 function Gallery(props) {
+  const [width, setWidth] = React.useState(window.innerWidth)
+  const [displayMovies, setDisplayMovies] = React.useState(0);
+  const [moviesToAdd, setMoviesToAdd] = React.useState(0);
+
+  React.useEffect(() => {
+    const onWindowResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    if (width <= 480) {
+      setDisplayMovies(5);
+      setMoviesToAdd(2)
+    } else if (width > 480 && width <= 768) {
+      setDisplayMovies(8);
+      setMoviesToAdd(2)
+    } else if (width > 768 && width <= 1280) {
+      setDisplayMovies(12);
+      setMoviesToAdd(3);
+    } else if (width > 1280) {
+      setDisplayMovies(16);
+      setMoviesToAdd(4);
+    }
+
+    window.addEventListener('resize', onWindowResize);
+    return () => {
+      window.removeEventListener('resize', onWindowResize);
+    };
+  }, [width]);
+
+  const handleClick = () => {
+    setDisplayMovies((oldValue) => oldValue + moviesToAdd);
+  };
+
   return (
     <section className="gallery">
 
@@ -13,7 +46,7 @@ function Gallery(props) {
       }
 
       <section className="gallery__elements">
-        {props.movies.map((movie) => (
+        {props.movies.slice(0, displayMovies).map((movie) => (
           <Movie
             isSavedMoviesPage={props.isSavedMoviesPage}
             onDeleteMovie={props.onDeleteMovie}
@@ -23,8 +56,8 @@ function Gallery(props) {
             key={movie.id} />
         ))}
       </section>
-      {props.showButton &&
-        <button className="gallery__button" type="button">Ещё</button>
+      {props.showButton && displayMovies < props.movies.length &&
+        <button className="gallery__button" type="button" onClick={handleClick}>Ещё</button>
       }
     </section>
   );
